fa63a32962e41b7df500d35f58a2175e03b029e4
Ceyda <ceydaonemli2@gmail.com>
2022-11-27 18:11:13 +0300

    listed categories and released this week

--- src/routes/Discover/components/Discover.tsx
+++ src/routes/Discover/components/Discover.tsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
 import '../styles/_discover.scss';
-import axios from   "axios"
+import axios from "axios"
 
 //TODO: Fix `any` types here
 
-interface IDiscoverProps {}
+interface IDiscoverProps { }
 
 interface IDiscoverState {
   newReleases: Array<any>;
@@ -22,47 +22,87 @@ export default class Discover extends Component<IDiscoverProps, IDiscoverState> 
       newReleases: [],
       playlists: [],
       categories: [],
-      token:"",
+      token: "",
     };
   }
 
   //TODO: Handle APIs
-   //playlists
+  //playlists
 
-   async componentDidMount() {
-    var spotify_client_id=process.env.REACT_APP_SPOTIFY_CLIENT_ID
-    var spotify_client_secret=process.env.REACT_APP_SPOTIFY_CLIENT_SECRET 
- 
-  //API access token
-  var authParameters={
-    method:'POST',
-    headers:{
-      'Content-Type':'application/x-www-form-urlencoded'
-    },
-    body:'grant_type=client_credentials&client_id='+spotify_client_id+'&client_secret='+spotify_client_secret
-  }
- const res= await fetch('https://accounts.spotify.com/api/token',authParameters)
- const data= await res.json()
-  this.setState({token:data.access_token})
-  console.log(data.access_token);
+  async componentDidMount() {
+    var spotify_client_id = process.env.REACT_APP_SPOTIFY_CLIENT_ID
+    var spotify_client_secret = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET
 
-  
- await axios("https://api.spotify.com/v1/browse/featured-playlists", {
-  method:"GET",
-  headers: {
-      Authorization: 'Bearer ' + this.state.token,       
-  },
-})
-       .then( res => {
-        const playlists =  res.data.playlists.items;
+    //API access token
+    var authParameters = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: 'grant_type=client_credentials&client_id=' + spotify_client_id + '&client_secret=' + spotify_client_secret
+    }
+    const res = await fetch('https://accounts.spotify.com/api/token', authParameters)
+    const data = await res.json()
+    this.setState({ token: data.access_token })
+    console.log(data.access_token);
+
+
+    await axios("https://api.spotify.com/v1/browse/featured-playlists", {
+      method: "GET",
+      headers: {
+        Authorization: 'Bearer ' + this.state.token,
+      },
+    })
+      .then(res => {
+        const playlists = res.data.playlists.items;
         console.log(res.data);
         this.setState({ playlists });
       }).catch((error) => {
         console.log("error", error.message);
       })
-      console.log(this.state);
+    console.log(this.state);
+
+
+
+    await axios("https://api.spotify.com/v1/browse/new-releases", {
+      method: "GET",
+      headers: {
+        Authorization: 'Bearer ' + this.state.token,
+      },
+    })
+      .then(res => {
+        const newReleases = res.data.albums.items
+        console.log(res.data.albums.items);
+        this.setState({ newReleases })
+      })
+      .catch((error) => {
+        console.log("error", error.message);
+      })
+    console.log(this.state);
+
+
+
+
+
+
+    await axios("https://api.spotify.com/v1/browse/categories", {
+      method: "GET",
+      headers: {
+        Authorization: 'Bearer ' + this.state.token,
+      },
+    })
+      .then(res => {
+        const categories = res.data.categories.items
+        console.log(res.data.categories.items);
+        this.setState({ categories })
+      })
+      .catch((error) => {
+        console.log("error", error.message);
+      })
+    console.log(this.state);
 
   }
+
 
   render() {
     const { newReleases, playlists, categories } = this.state;
